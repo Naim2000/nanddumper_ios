de3cdbac6627bf79350a178d1001134d82ba616f
Naim2000 <naimarnohero@gmail.com>
2025-06-13 11:26:14 -0500

    Credits for xyzzy-mod

--- source/vwii_sram_otp.h
+++ source/vwii_sram_otp.h
@@ -1,3 +1,10 @@
+/*
+ * From xyzzy-mod by DarkMatterCore. Original xyzzy by bushing
+ * https://github.com/DarkMatterCore/xyzzy-mod/blob/master/source/vwii_sram_otp.h
+ *
+ * Sort of, anyways. The vwii_sram_otp_read() business is a waste of time
+ */
+
 #ifndef __VWII_SRAM_OTP_H__
 #define __VWII_SRAM_OTP_H__
 
@@ -14,7 +21,7 @@ typedef struct {
     u8 nss_device_cert[32];
 } vwii_sram_otp_t;
 
-static const vwii_sram_otp_t* const vwii_sram_otp = (const vwii_sram_otp_t *)0xCD407F00;
+static const vwii_sram_otp_t* const vwii_sram_otp = (const vwii_sram_otp_t *)0xCD407F00; // not volatile. not changing any time soon ha
 
 // u16 vwii_sram_otp_read(void *dst, u16 offset, u16 size);
 
